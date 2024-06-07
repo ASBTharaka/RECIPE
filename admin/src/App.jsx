a8a0aa65d0374c3bcbe236613acb2c5b258e2ac7
ASBTharaka <tharakarathnayake6@gmail.com>
2024-06-07 12:30:48 +0530

    admin pannel complete

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,6 +10,7 @@ import { ToastContainer} from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const url = "http://localhost:4000";
   return (
     <div>
       <ToastContainer/>
@@ -18,9 +19,9 @@ const App = () => {
       <div className="app-content">
         <Sidebar/>
         <Routes>
-          <Route path="/add" element={<Add/>}/>
-          <Route path="/list" element={<List/>}/>
-          <Route path="/order" element={<Order/>}/>
+          <Route path="/add" element={<Add url={url}/>}/>
+          <Route path="/list" element={<List url={url}/>}/>
+          <Route path="/order" element={<Order url={url}/>}/>
         </Routes>
       </div>
     </div>
--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
-import './List.css'
+import React, { useEffect, useState } from 'react';
+import './List.css';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const List = () => {
+const List = ({url}) => {
+  
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      if (response.data.success) {
+        setList(response.data.data);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Error fetching list");
+    }
+  };
+
+  const removeFood = async (foodId) => {
+    try {
+      const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchList();
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Error removing food");
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return (
-    <div>List</div>
-  )
-}
+    <div className='list add flex-col'>
+      <ToastContainer />
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b><b>Name</b><b>Category</b><b>Price</b><b>Action</b>
+        </div>
+        {list.map((item, index) => (
+          <div key={index} className='list-table-format'>
+            <img src={`${url}/images/${item.image}`} alt={item.name} />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>${item.price.toFixed(2)}</p>
+            <p onClick={() => removeFood(item._id)} className='cursor'>X</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;
